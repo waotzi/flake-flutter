dc13a0c7063f3a6e852678f56d41589c2cd139cd
waotzi <waotzi@proton.me>
2023-08-17 07:34:44 +0000

    update version

--- template/flake.nix
+++ template/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flutter-flake = {
-      url    = "github:waotzi/flake-flutter/3.1.1";
+      url    = "github:waotzi/flake-flutter/3.1.0";
       inputs.nixpkgs.follows  = "nixpkgs";
     };
 
