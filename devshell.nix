8b74acbf777ce797c558b015de1a25e92ac82fbe
waotzi <waotzi@proton.me>
2023-08-17 07:34:24 +0000

    rm audio changes

--- devshell.nix
+++ devshell.nix
@@ -66,13 +66,6 @@ devshell.mkShell {
     clang
     cmake
 
-    libunwind
-    orc
-    gst_all_1.gstreamer
-    gst_all_1.gst-libav
-    gst_all_1.gst-plugins-base
-    gst_all_1.gst-plugins-good
-
     flutter
     glib
     glib.dev
